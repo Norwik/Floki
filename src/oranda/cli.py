47d08762c2e477ba350d281b0b2826172cf16c1d
Clivern <hello@clivern.com>
2022-01-31 18:28:22 +0100

    init

--- src/oranda/cli.py
+++ src/oranda/cli.py
@@ -33,29 +33,66 @@ def main():
 def host():
     pass
 
-@host.command(help='Get a host')
-def get():
-    click.echo('Get host')
-
 @host.command(help='List hosts')
 @click.option("-t", "--tag", "tag", type=click.STRING, default="", help="Host tags")
-def list(tag):
+@click.option("-o", "--output", "output", type=click.STRING, default="", help="Output format")
+def list(tag, output):
     if tag:
         click.echo(f'List hosts with tag {tag}')
     else:
         click.echo('List all hosts')
 
+@host.command(help='Add a host')
+@click.argument('name')
+@click.option("-c", "--connection", "connection", type=click.STRING, default="ssh", help="Connection type to the host")
+@click.option("-h", "--host", "host", type=click.STRING, default="", help="The name of the host to connect to")
+@click.option("-p", "--port", "port", type=click.INT, default=22, help="The connection port number")
+@click.option("-u", "--user", "user", type=click.STRING, default="root", help="The user name to use when connecting to the host")
+@click.option("-pa", "--password", "password", type=click.STRING, default="", help="The password to use to authenticate to the host")
+@click.option("-s", "--ssh_private_key_file", "ssh_private_key_file", type=click.File(), default="", help="Private key file used by ssh")
+@click.option("-t", "--tag", "tag", type=click.STRING, default="", help="Host tags")
+@click.option("-o", "--output", "output", type=click.STRING, default="", help="Output format")
+def add(name, connection, host, port, user, password, ssh_private_key_file, tag, output):
+    click.echo(f'Add host {name}')
+
+@host.command(help='Get a host')
+@click.argument('name')
+@click.option("-o", "--output", "output", type=click.STRING, default="", help="Output format")
+def get(name, output):
+    click.echo(f'Get host {name}')
+
 @host.command(help='Delete a host')
-def delete():
-    click.echo('Delete host')
+def delete(name):
+    click.echo(f'Delete host {name}')
+
 
 @click.group(help='Manage recipes')
 def recipe():
     pass
 
+@recipe.command(help='Add a recipe')
+@click.argument('name')
+@click.option("-p", "--path", "path", type=click.Path(exists=True), default="", help="Path to the recipe")
+def add(name, path):
+    click.echo(f'Add recipe {name}')
+
 @recipe.command(help='List all recipes')
-def list():
-    click.echo('List recipes')
+@click.option("-t", "--tag", "tag", type=click.STRING, default="", help="Recipe tags")
+def list(tag):
+    if tag:
+        click.echo(f'List recipes with tag {tag}')
+    else:
+        click.echo('List all recipes')
+
+@recipe.command(help='Get a recipe')
+@click.argument('name')
+@click.option("-o", "--output", "output", type=click.STRING, default="", help="Output format")
+def get(name, output):
+    click.echo(f'Get recipe {name}')
+
+@recipe.command(help='Delete a recipe')
+def delete(name):
+    click.echo(f'Delete recipe {name}')
 
 main.add_command(host)
 main.add_command(recipe)
